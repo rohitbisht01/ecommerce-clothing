70b501d7400dba3ab3a68934cc4ca0c58af95d3c
rohitbisht01 <rb9519208@gmail.com>
2023-02-10 19:23:07 +0530

    updated menu-items to navigate withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
 import HomePage from "./pages/home/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import "./index.css"
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-
-reportWebVitals();
